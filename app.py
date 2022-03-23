f73248fa197eb9512a8ce782acd2cb614a216922
Jasmine Ly <jasminely428@gmail.com>
2022-03-23 07:59:52 -0400

    WTF PROGRESS

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
 from flask import Flask, render_template
+import random
 
+starNouns = []
+starAdjs = []
+
+with open('stars_nouns.txt', 'r') as f:
+  for line in f:
+    starNouns.append(line.strip())
+
+with open('stars_adjectives.txt', 'r') as t:
+  for line in t:
+    starAdjs.append(line.strip())
 
 app = Flask(__name__)
 
@@ -9,7 +20,7 @@ app = Flask(__name__)
 
 def index():
 
-
+#--------------USER VISITS-------------------
     # Load current count
     f = open("count.txt", "r")
     count = int(f.read())
@@ -19,13 +30,41 @@ def index():
     # Increment the count
     count += 1
 
+
     # Overwrite the count
     f = open("count.txt", "w")
     f.write(str(count))
     f.close()
+#--------------USER VISITS-------------------
+
+#--------------STAR GAZING-------------------
+    f = open("user_stargaze.txt", "r")
+    user_stargaze = str(f.read())
+    f.close()
+
+    user_stargaze = (starAdjs[random.randint(0, len(starAdjs)-1)]) + "\n" + (starNouns[random.randint(0, len(starNouns)-1)]) 
+
+    f = open("user_stargaze.txt", "w")
+    f.write(str(user_stargaze))
+    f.close()
+
+    
+    f = open("stargaze.txt", "r")
+    stargaze = str(f.read())
+    f.close()
+
+    
+    
+    if count%2==0:
+        f = open("stargaze.txt", "w")
+        f.write(str(user_stargaze))
+        f.close()
+
+
+#--------------STAR GAZING-------------------
 
     # Render HTML with count variable
-    return render_template("index.html", count=count)
+    return render_template("index.html", count=count, user_stargaze=user_stargaze, stargaze=stargaze)
 
 
 if __name__ == "__main__":
