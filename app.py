c81c96b2b6c5bf94da15b9ccd11b58612b96b75f
Jasmine Ly <jasminely428@gmail.com>
2022-03-23 05:36:52 -0400

    button test

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response, request, redirect, url_for
+
 
 app = Flask(__name__)
 
@@ -6,11 +7,26 @@ app = Flask(__name__)
 
 
 def index():
+   
+
+    m = open("marshmellow.txt", "r")
+    marshmellowCount = int(m.read())
+    m.close()
+
+    @app.route("/roast/", methods=['POST'])
+    def roastButtonClick():
+        marshmellowCount +=1
+        return render_template('index.html', marshmellowCount=marshmellowCount)
+
+    m - open("marshmellow.txt", "w")
+    m.write(str(marshmellowCount))
+    m.close()
 
     # Load current count
     f = open("count.txt", "r")
     count = int(f.read())
     f.close()
+
 
     # Increment the count
     count += 1
@@ -22,6 +38,7 @@ def index():
 
     # Render HTML with count variable
     return render_template("index.html", count=count)
+
 
 if __name__ == "__main__":
     app.run()
